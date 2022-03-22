c90bafaa971d81fe2f02c9cf8f6b8f643c51ebf1
Kevin <kayaswp@gmail.com>
2022-03-22 21:12:02 +0630

    fetch pj and show in HomeView and make a SingleProject component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,37 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <div v-for="project in projects" :key="project.id">
+      <SingleProject :project='project'></SingleProject>
+    </div>
   </div>
 </template>
 
 <script>
 
 
+import SingleProject from '../components/SingleProject'
 export default {
   name: 'HomeView',
+  data(){
+    return{
+      projects:[]
+    }
+  },
   components: {
-    
+    SingleProject,
+  },
+  mounted(){
+    fetch('http://localhost:3000/projects')
+    .then((response)=>{
+      return response.json()
+    })
+    .then((datas)=>{
+      this.projects = datas
+    })
+    .catch(()=>{
+
+    })
   }
 }
 </script>
