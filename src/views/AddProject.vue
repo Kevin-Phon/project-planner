c561f0750293f5ce399b6107e6f8b1f243cd1b46
Kevin <kayaswp@gmail.com>
2022-03-26 19:59:16 +0630

    dynamic edit project route and fetch all edit datas from api

--- src/views/AddProject.vue
+++ src/views/AddProject.vue
@@ -1,17 +1,45 @@
 <template>
   <h1>Add Project</h1>
-  <form>
+  <form @submit.prevent="addProject">
       <label>Project Title</label>
-      <input type="text">
+      <input type="text" v-model="title">
       <label>Project Detail</label>
-      <input type="text">
+      <input type="text" v-model="detail">
       <button>Add</button>
   </form>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            title: "",
+            detail: ""
+        }
+    },
+    methods:{
+        addProject(){
+            fetch('http://localhost:3000/projects',{
+                method:"POST",
+                headers:{
+                    "Content-Type" : "application/json"
+                },
+                body:JSON.stringify(
+                {
+                    title:this.title,
+                    detail:this.detail,
+                    complete:false
+                }
+            )
+        })
+        .then(()=>{
+            this.$router.push('/')
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    }
+}
 }
 </script>
 
@@ -53,5 +81,6 @@ export default {
         border: 0;
         border-radius: 6px;
         font-size: 16px;
+        cursor: pointer;
     }
 </style>
